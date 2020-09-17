6cb92fe7202650bf6fb765c5d6fa12863c1e267a
dudor <dudor@qq.com>
2020-09-18 07:27:42 +0800

    gogogo

--- react/realworld/src/components/Article/index.js
+++ react/realworld/src/components/Article/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import agent from '../../agent'
 import marked from 'marked'
 import ArticleMeta from './ArticleMeta'
+import CommentContainer from './CommentContainer'
+import { connect } from 'react-redux'
 const mapStateToProps = state => {
     return {
         ...state.Article,
@@ -43,8 +45,49 @@ class Article extends React.Component {
                             canModify={canModify} />
                     </div>
                 </div>
+
+                <div className='container page'>
+                    <div className='row article-content'>
+                        <div className='col-xs-12'>
+                            <div dangerouslySetInnerHTML={markup}>
+
+                            </div>
+
+                            <ul className='tag-list'>
+                                {
+                                    this.props.article.tagList.map(tag => {
+                                        return (
+                                            <li className='tag-default tag-pill tag-outline' key={tag}>
+                                                {tag}
+                                            </li>
+                                        )
+                                    })
+                                }
+
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    <hr />
+
+                    <div className='article-actions'>
+                    </div>
+
+                    <div className='row'>
+                        <CommentContainer comments={this.props.comments || []}
+                            errors={this.props.commentErrors}
+                            slut={this.props.params.id}
+                            currentUser={this.props.currentUser}>
+                        </CommentContainer>
+                    </div>
+
+                </div>
+
             </div>
         )
 
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Article)
